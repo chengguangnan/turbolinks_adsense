2d5f91c7ead761b3cdc202d606948b49680b8d6f
Cheng Guangnan <guangnan@factual.com>
2013-01-16 20:37:39 +0800

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurbolinksAdsense::Application.config.secret_token = '0ab1edda3091e7f7e8ba05a984949cd09c228e386289b780b0c3e08f7ca5afef527285202e95bdaad6a399605bf3daaf422d2f8778e205ec31305ee6dd118247'
